7fc90fd18751ed676705e4c21b01ad490bfb928d
freelanceradnan <reactorbro722@gmail.com>
2026-07-04 18:22:51 +0600

    read create rename and checkout added

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-
-const fs = require('fs');
-
-function readData() {
-    try {
-        const data = fs.readFileSync('./adnan.text', 'utf-8');
-        console.log(data);
-    } catch (error) {
-        console.error("ফাইলটি পড়া যায়নি:", error);
-    }
+const fs=require('fs')
+//read file 
+async function ReadFile(){
+    const data=await fs.readFileSync('todo2.txt','utf-8')
+    console.log(data)
 }
 
-readData();
+//create new file
+async function CreateFile(){
+    const data=await fs.writeFileSync('adnan.text','hello adnan')
+    console.log('file created and read done')
+}
+
+//rename the file
+async function RenameFile(){
+    const data=await fs.renameSync('todo2.txt','rename2.txt')
+    console.log('rename success!')
+}
+
+//checkout exists
+async function existsFileCheckout(){
+    const data=await fs.existsSync('rename2.txt')
+    console.log(data)
+}
+(async()=>{
+    await existsFileCheckout()
+})()
